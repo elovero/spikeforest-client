a4857bdccae49d1ffad49a8dbc7d3225a7691286
Elizabeth Lovero <elovero@flatironinstitute.org>
2019-01-31 13:42:39 -0500

    Update basic build."

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import { async } from "rxjs/internal/scheduler/async";
-import styled from 'styled-components';
 
 class App extends Component {
   state = {
