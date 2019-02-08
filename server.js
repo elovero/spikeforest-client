cf341fe96fd17110e15d555e45d6aabc90dbb895
Elizabeth Lovero <elovero@flatironinstitute.org>
2019-02-07 23:23:53 -0500

    Update Sentry config.

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const Sentry = require('@sentry/node');
+Sentry.init({
+  dsn: 'https://a7b7f1b624b44a9ea537ec1069859393@sentry.io/1365884',
+});
 
 // Express server setup
 const express = require('express');
